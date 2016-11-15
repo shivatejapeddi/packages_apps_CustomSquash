ea90f86e554459645322ce69444507e8539425da
Adarsh-MR <adarshmr1998@gmail.com>
2016-11-15 19:01:07 +0000

    Revert "CustomSquash: Citrus-CAF Logo on Status Bar (2/2)"
    This reverts commit 764c53211b25ddf4be50ae2c0d1acc1b11e2ab07.
    
    Conflicts:
    	res/values/arrays.xml
    	res/values/strings.xml
    	res/xml/statusbar_tab.xml

--- src/com/citrus/settings/tabs/StatusBar.java
+++ src/com/citrus/settings/tabs/StatusBar.java
@@ -34,16 +34,8 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
-
 public class StatusBar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
-
-    private static final String KEY_CITRUS_LOGO_COLOR = "status_bar_citrus_logo_color";
-    private static final String KEY_CITRUS_LOGO_STYLE = "status_bar_citrus_logo_style";
-
-    private ColorPickerPreference mCitrusLogoColor;
-    private ListPreference mCitrusLogoStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,26 +43,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.statusbar_tab);
 
-        PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
-
-        mCitrusLogoStyle = (ListPreference) findPreference(KEY_CITRUS_LOGO_STYLE);
-        int citrusLogoStyle = Settings.System.getIntForUser(resolver,
-                Settings.System.STATUS_BAR_CITRUS_LOGO_STYLE, 0,
-                UserHandle.USER_CURRENT);
-        mCitrusLogoStyle.setValue(String.valueOf(citrusLogoStyle));
-        mCitrusLogoStyle.setSummary(mCitrusLogoStyle.getEntry());
-        mCitrusLogoStyle.setOnPreferenceChangeListener(this);
-
-        // Citrus-CAF logo color
-        mCitrusLogoColor =
-            (ColorPickerPreference) prefSet.findPreference(KEY_CITRUS_LOGO_COLOR);
-        mCitrusLogoColor.setOnPreferenceChangeListener(this);
-        int intColor = Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_CITRUS_LOGO_COLOR, 0xffffffff);
-        String hexColor = String.format("#%08x", (0xffffffff & intColor));
-        mCitrusLogoColor.setSummary(hexColor);
-        mCitrusLogoColor.setNewPreviewColor(intColor);
     }
 
     @Override
@@ -89,25 +62,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-    ContentResolver resolver = getActivity().getContentResolver();
         final String key = preference.getKey();
-         if (preference == mCitrusLogoColor) {
-            String hex = ColorPickerPreference.convertToARGB(
-                    Integer.valueOf(String.valueOf(objValue)));
-            preference.setSummary(hex);
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(resolver,
-                    Settings.System.STATUS_BAR_CITRUS_LOGO_COLOR, intHex);
-            return true;
-        } else if (preference == mCitrusLogoStyle) {
-            int citrusLogoStyle = Integer.valueOf((String) objValue);
-            int index = mCitrusLogoStyle.findIndexOfValue((String) objValue);
-            Settings.System.putIntForUser(
-                    resolver, Settings.System.STATUS_BAR_CITRUS_LOGO_STYLE, citrusLogoStyle,
-                    UserHandle.USER_CURRENT);
-            mCitrusLogoStyle.setSummary(mCitrusLogoStyle.getEntries()[index]);
-            return true;
-        }
         return true;
     }
 }
