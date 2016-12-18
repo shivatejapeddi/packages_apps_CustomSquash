7135abbc8bbc5eff1f60995347dd74e7c6c8e14a
rohitporoli <rohitp4all@gmail.com>
2016-12-18 19:43:27 +0530

    CustomSquash: Move Custom Summary to Misc
    * Garnish --> Misc
    * Make things look dope <3
    * Add #HakunaMatata
    * Fix a few stuff here and there
    
    Signed-off-by: rohitporoli <rohitp4all@gmail.com>

---
+++ src/com/citrus/settings/tabs/Misc.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2016 Citrus-CAF Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.citrus.settings.tabs;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.text.Spannable;
+import android.text.TextUtils;
+import android.widget.EditText;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.Utils;
+
+public class Misc extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_CUSTOM_SETTINGS_SUMMARY = "custom_settings_summary";
+
+    private Preference mCustomSummary;
+    private String mCustomSummaryText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.misc_tab);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+        mCustomSummary = (Preference) findPreference(PREF_CUSTOM_SETTINGS_SUMMARY);
+        updateCustomSummaryTextString();
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.CUSTOM_SQUASH;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+     @Override
+     public boolean onPreferenceTreeClick(Preference preference) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mCustomSummary) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+            alert.setTitle(R.string.custom_summary_title);
+            alert.setMessage(R.string.custom_summary_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(TextUtils.isEmpty(mCustomSummaryText) ? "" : mCustomSummaryText);
+            input.setSelection(input.getText().length());
+            alert.setView(input);
+            alert.setPositiveButton(getString(android.R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            String value = ((Spannable) input.getText()).toString().trim();
+                            Settings.System.putString(resolver, Settings.System.CUSTOM_SETTINGS_SUMMARY, value);
+                            updateCustomSummaryTextString();
+                        }
+                    });
+            alert.setNegativeButton(getString(android.R.string.cancel), null);
+            alert.show();
+        } else {
+            return super.onPreferenceTreeClick(preference);
+        }
+        return false;
+     }
+
+    private void updateCustomSummaryTextString() {
+        mCustomSummaryText = Settings.System.getString(
+                getActivity().getContentResolver(), Settings.System.CUSTOM_SETTINGS_SUMMARY);
+
+        if (TextUtils.isEmpty(mCustomSummaryText)) {
+            mCustomSummary.setSummary(R.string.custom_title_summary);
+        } else {
+            mCustomSummary.setSummary(mCustomSummaryText);
+        }
+    }
+}
