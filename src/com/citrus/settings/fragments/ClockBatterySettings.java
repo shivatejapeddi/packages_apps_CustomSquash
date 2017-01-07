b18e5d10c50bf9f5ffd49cd2b469dc0d443a3ece
Alex Cruz <mazdarider23@gmail.com>
2017-01-07 09:34:55 +0000

    Turn off 'forced battery percent while charging' default [2/2]

--- src/com/citrus/settings/fragments/ClockBatterySettings.java
+++ src/com/citrus/settings/fragments/ClockBatterySettings.java
@@ -152,7 +152,7 @@ public class ClockBatterySettings extends SettingsPreferenceFragment implements
 
         mForceChargeBatteryText = (SwitchPreference) findPreference(FORCE_CHARGE_BATTERY_TEXT);
         mForceChargeBatteryText.setChecked((Settings.Secure.getInt(resolver,
-                Settings.Secure.FORCE_CHARGE_BATTERY_TEXT, 1) == 1));
+                Settings.Secure.FORCE_CHARGE_BATTERY_TEXT, 0) == 1));
         mForceChargeBatteryText.setOnPreferenceChangeListener(this);
 
         mStatusBarBatteryShowPercent =
