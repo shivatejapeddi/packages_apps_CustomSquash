09a2d77e95d8c4c1e5df7444cbe8693d14ae9ebd
Lars Greiss <kufikugel@googlemail.com>
2017-06-17 18:05:51 +0000

    HeadsUp: add timeout option (2/2)
    Forward ported to marshmallow/nougat By: beanstown106
    removed the none option as we now have a headsup minimum time of 2seconds
    added 5sec option as that is the new default in marshmallow
    
    Signed-off-by: rohitporoli <rohitp4all@gmail.com>

--- src/com/citrus/settings/fragments/HeadsUpSettings.java
+++ src/com/citrus/settings/fragments/HeadsUpSettings.java
@@ -16,8 +16,11 @@
 
 package com.citrus.settings.fragments;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,7 +35,11 @@ import com.citrus.settings.utils.Utils;
 import com.citrus.settings.BaseSystemSettingSwitchBar;
 
 public class HeadsUpSettings extends SettingsPreferenceFragment
-        implements BaseSystemSettingSwitchBar.SwitchBarChangeCallback {
+        implements BaseSystemSettingSwitchBar.SwitchBarChangeCallback,  Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
+
+    private ListPreference mHeadsUpTimeOut;
 
     private BaseSystemSettingSwitchBar mEnabledSwitch;
 
@@ -44,6 +51,21 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         super.onCreate(savedInstanceState);
         // Get launch-able applications
         addPreferencesFromResource(R.xml.headsup_settings);
+        Resources systemUiResources;
+        try {
+            systemUiResources = getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (Exception e) {
+            return;
+        }
+
+        int defaultTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_notification_decay", null, null));
+        mHeadsUpTimeOut = (ListPreference) findPreference(PREF_HEADS_UP_TIME_OUT);
+        mHeadsUpTimeOut.setOnPreferenceChangeListener(this);
+        int headsUpTimeOut = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT, defaultTimeOut);
+        mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
+        updateHeadsUpTimeOutSummary(headsUpTimeOut);
     }
 
     @Override
@@ -102,9 +124,28 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         return MetricsEvent.CUSTOM_SQUASH;
     }
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHeadsUpTimeOut) {
+            int headsUpTimeOut = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_TIMEOUT,
+                    headsUpTimeOut);
+            updateHeadsUpTimeOutSummary(headsUpTimeOut);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateHeadsUpTimeOutSummary(int value) {
+        String summary = getResources().getString(R.string.heads_up_time_out_summary,
+                value / 1000);
+        mHeadsUpTimeOut.setSummary(summary);
+    }
+
     private boolean getUserHeadsUpState() {
          return Settings.System.getIntForUser(getContentResolver(),
-                SSettings.System.HEADS_UP_USER_ENABLED,
+                Settings.System.HEADS_UP_USER_ENABLED,
                 Settings.System.HEADS_UP_USER_ON,
                 UserHandle.USER_CURRENT) != 0;
     }
