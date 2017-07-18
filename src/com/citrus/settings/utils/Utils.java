d9507805305190f4ada2936cba2860ce1ead908d
Udam Saini <udam@google.com>
2017-07-18 06:53:45 +0000

    add isDeviceProvisioned into utils
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- src/com/citrus/settings/utils/Utils.java
+++ src/com/citrus/settings/utils/Utils.java
@@ -32,6 +32,7 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.net.ConnectivityManager;
 import android.os.UserManager;
+import android.provider.Settings; 
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.view.DisplayInfo;
@@ -204,5 +205,10 @@ public final class Utils {
             // Ignore
         }
         return false;
+    }
+
+    public static boolean isDeviceProvisioned(Context context) {
+        return Settings.Global.getInt(context.getContentResolver(),
+                Settings.Global.DEVICE_PROVISIONED, 0) != 0;
     }
 }
