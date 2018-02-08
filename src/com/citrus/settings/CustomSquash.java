0aadc7e678fd0d8062c0fc1ef8fdd2fdb0933634
LorDClockaN <lordclockan@gmail.com>
2018-02-08 21:11:27 +0000

    CustomSquash: Custom summary [1/2]

--- src/com/citrus/settings/CustomSquash.java
+++ src/com/citrus/settings/CustomSquash.java
@@ -1,15 +1,22 @@
 package com.citrus.settings;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 
+import android.provider.Settings;
+
+import android.text.TextUtils;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -22,6 +29,8 @@ import com.citrus.settings.tabs.LockScreen;
 import com.citrus.settings.PagerSlidingTabStrip;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import java.util.Random;
 
 public class CustomSquash extends SettingsPreferenceFragment {
 
@@ -104,6 +113,53 @@ public class CustomSquash extends SettingsPreferenceFragment {
             getString(R.string.misc_title)};
         return titleString;
     }
+
+    private static class SummaryProvider implements SummaryLoader.SummaryProvider {
+
+        private final Context mContext;
+        private final SummaryLoader mSummaryLoader;
+
+        public SummaryProvider(Context context, SummaryLoader summaryLoader) {
+            mContext = context;
+            mSummaryLoader = summaryLoader;
+        }
+
+        @Override
+        public void setListening(boolean listening) {
+            String mCustomSummary = Settings.System.getString(
+                    mContext.getContentResolver(), Settings.System.CUSTOM_SETTINGS_SUMMARY);
+            boolean mRandSum = Settings.System.getInt(
+                    mContext.getContentResolver(), Settings.System.CUSTOM_SETTINGS_RANDOM_SUMMARY, 0) == 1;
+            final String[] summariesArray = mContext.getResources().getStringArray(R.array.custom_summaries);
+            String chosenSum = randomSummary(summariesArray);
+
+            if (listening) {
+                if (TextUtils.isEmpty(mCustomSummary) && !mRandSum) {
+                    mSummaryLoader.setSummary(this, mContext.getString(R.string.custom_title_summary));
+                } else if (!TextUtils.isEmpty(mCustomSummary) && !mRandSum) { //Random is off, Use User's input
+                    mSummaryLoader.setSummary(this, mCustomSummary);
+                } else if (TextUtils.isEmpty(mCustomSummary) && mRandSum) { //Random is on, User Input is blank
+                    mSummaryLoader.setSummary(this, chosenSum);
+                } else if (!TextUtils.isEmpty(mCustomSummary) && mRandSum) { //Random is on, but User has input
+                    mSummaryLoader.setSummary(this, chosenSum); //Override Text from user input
+                }
+            }
+       }
+
+        public static String randomSummary(String[] array) {
+            int rand = new Random().nextInt(array.length);
+            return array[rand];
+        }
+   }
+
+    public static final SummaryLoader.SummaryProviderFactory SUMMARY_PROVIDER_FACTORY
+            = new SummaryLoader.SummaryProviderFactory() {
+        @Override
+        public SummaryLoader.SummaryProvider createSummaryProvider(Activity activity,
+                                                                   SummaryLoader summaryLoader) {
+            return new SummaryProvider(activity, summaryLoader);
+        }
+    };
 
     @Override
     public int getMetricsCategory() {
--- src/com/citrus/settings/tabs/Misc.java
+++ src/com/citrus/settings/tabs/Misc.java
@@ -16,11 +16,20 @@
 
 package com.citrus.settings.tabs;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.provider.Settings;
+
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.text.Spannable;
+import android.text.TextUtils;
+import android.widget.EditText;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -28,7 +37,12 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 public class Misc extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+ 
+    private static final String PREF_CUSTOM_SETTINGS_SUMMARY = "custom_settings_summary"; 
 
+    private Preference mCustomSummary;
+    private String mCustomSummaryText;
+ 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +52,8 @@ public class Misc extends SettingsPreferenceFragment implements
 
         ContentResolver resolver = getActivity().getContentResolver();
 
+        mCustomSummary = (Preference) findPreference(PREF_CUSTOM_SETTINGS_SUMMARY);
+        updateCustomSummaryTextString();
     }
 
     @Override
@@ -59,8 +75,43 @@ public class Misc extends SettingsPreferenceFragment implements
         return true;
     }
 
-     @Override
+    @Override
     public boolean onPreferenceTreeClick(Preference preference) {
-        return super.onPreferenceTreeClick(preference);
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mCustomSummary) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+            alert.setTitle(R.string.custom_summary_title);
+            alert.setMessage(R.string.custom_summary_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(TextUtils.isEmpty(mCustomSummaryText) ? "" : mCustomSummaryText);
+            input.setSelection(input.getText().length());
+            alert.setView(input);
+            alert.setPositiveButton(getString(android.R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            String value = ((Spannable) input.getText()).toString().trim();
+                            Settings.System.putString(resolver, Settings.System.CUSTOM_SETTINGS_SUMMARY, value);
+                            updateCustomSummaryTextString();
+                        }
+                    });
+            alert.setNegativeButton(getString(android.R.string.cancel), null);
+            alert.show();
+        } else {
+            return super.onPreferenceTreeClick(preference);
+        }
+        return false;
+     }
+
+    private void updateCustomSummaryTextString() {
+        mCustomSummaryText = Settings.System.getString(
+                getActivity().getContentResolver(), Settings.System.CUSTOM_SETTINGS_SUMMARY);
+
+        if (TextUtils.isEmpty(mCustomSummaryText)) {
+            mCustomSummary.setSummary(R.string.custom_title_summary);
+        } else {
+            mCustomSummary.setSummary(mCustomSummaryText);
+        }
     }
 }
