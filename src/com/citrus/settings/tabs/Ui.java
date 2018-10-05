8a0dd81794c3463e658dd84976fc1e7ef2c7ecfd
Adarsh-MR <adarshmr1998@gmail.com>
2018-10-05 20:04:59 +0000

    CustomSquash: Re-enable Screen off animation options
    * Migrate it to use SystemSettingListPreference
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- src/com/citrus/settings/tabs/Ui.java
+++ src/com/citrus/settings/tabs/Ui.java
@@ -21,7 +21,6 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
@@ -41,10 +40,6 @@ import java.util.List;
 public class Ui extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
 
-    private static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
-
-    private ListPreference mScreenOffAnimation;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,14 +48,7 @@ public class Ui extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefScreen = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
-/*
-        mScreenOffAnimation = (ListPreference) findPreference(SCREEN_OFF_ANIMATION);
-        int screenOffStyle = Settings.System.getInt(resolver,
-                Settings.System.SCREEN_OFF_ANIMATION, 0);
-        mScreenOffAnimation.setValue(String.valueOf(screenOffStyle));
-        mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntry());
-        mScreenOffAnimation.setOnPreferenceChangeListener(this);
-*/   }
+    }
 
     @Override
     public int getMetricsCategory() {
@@ -79,16 +67,7 @@ public class Ui extends SettingsPreferenceFragment implements
 
 
      public boolean onPreferenceChange(Preference preference, Object objValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-/*        if (preference == mScreenOffAnimation) {
-            String value = (String) objValue;
-            Settings.System.putInt(resolver,
-                    Settings.System.SCREEN_OFF_ANIMATION, Integer.valueOf(value));
-            int valueIndex = mScreenOffAnimation.findIndexOfValue(value);
-            mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntries()[valueIndex]);
-            return true;
-        }*/
-        return false;
+        return true;
      }
 
     public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
